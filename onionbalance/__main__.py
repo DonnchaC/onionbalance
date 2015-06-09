4935ff20d407672e35ea7fc50f1e2c8571eaea94
Donncha O'Cearbhaill <donncha@donncha.is>
2015-06-09 19:40:20 +0100

    Add convenience wrapper for running service

---
+++ onionbalance/__main__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from onionbalance.manager import main
+
+main()
--- onionbalance/manager.py
+++ onionbalance/manager.py
@@ -157,6 +157,3 @@ def main():
         logger.info("Stopping descriptor uploads")
 
     return 0
-
-if __name__ == "__main__":
-    exit(main())
