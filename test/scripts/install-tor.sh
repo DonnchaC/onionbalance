138be82ae07c637613b27c517e4e11f202ee6d0e
Donncha O'Cearbhaill <donncha@donncha.is>
2015-06-24 13:46:06 +0100

    Add Tor to PATH rather than running make install

--- test/scripts/install-tor.sh
+++ test/scripts/install-tor.sh
@@ -2,5 +2,5 @@
 # Script to install Tor
 set -ex
 wget https://www.torproject.org/dist/tor-0.2.7.1-alpha.tar.gz
-tar -xzvf tor-0.2.7.1-alpha.tar.gz
-cd tor-0.2.7.1-alpha && ./configure --disable-asciidoc && make && make install
+tar -xzvf tor-0.2.7.1-alpha.tar.gz && mv tor-0.2.7.1-alpha tor
+cd tor && ./configure --disable-asciidoc && make
