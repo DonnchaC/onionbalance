b172edf63ac0e70b2a520aaa5288cf1867d2ab2e
Donncha O'Cearbhaill <donncha@donncha.is>
2015-06-24 13:23:18 +0100

    Remove unneccesary sudo command

--- test/scripts/install-tor.sh
+++ test/scripts/install-tor.sh
@@ -3,4 +3,4 @@
 set -ex
 wget https://www.torproject.org/dist/tor-0.2.7.1-alpha.tar.gz
 tar -xzvf tor-0.2.7.1-alpha.tar.gz
-cd tor-0.2.7.1-alpha && ./configure --disable-asciidoc && make && sudo make install
+cd tor-0.2.7.1-alpha && ./configure --disable-asciidoc && make && make install
