ec36e33751d14a0ab53bbf4b32490941df2461f5
Donncha O'Cearbhaill <donncha@donncha.is>
2015-07-01 13:01:17 +0100

    Bug fix release, bump version and changelog

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,3 +1,10 @@
+0.1.1
+-----
+
+- Patch to resolve issue when saving generated torrc files from
+  onionbalance-config in Python 2.
+
+
 0.1.0
 -----
 
--- onionbalance/__init__.py
+++ onionbalance/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Donncha O'Cearbhaill"
 __contact__ = "donncha@donncha.is"
 __url__ = "https://github.com/DonnchaC/onionbalance"
