6a8c58d4b19ab5b23d27037e73c767108d66a86a
Donncha O'Cearbhaill <donncha@donncha.is>
2017-02-17 16:39:07 +0100

    Only run descriptor fetch once per unique instance

--- onionbalance/instance.py
+++ onionbalance/instance.py
@@ -31,17 +31,25 @@ def fetch_instance_descriptors(controller):
         else:
             break
 
-    for service in config.services:
-        for instance in service.instances:
-            while True:
-                try:
-                    instance.fetch_descriptor()
-                except stem.SocketClosed:
-                    logger.error("Failed to fecth descriptor, socket "
-                                 "is closed")
-                    util.reauthenticate(controller, logger)
-                else:
-                    break
+    unique_instances = set(instance for service in config.services
+                           for instance in service.instances)
+
+    # Only try to retrieve the descriptor once for each unique instance
+    # address. An instance may be configured under multiple master
+    # addressed. We do not want to request the same instance descriptor
+    # multiple times.
+    # OnionBalance will update all of the matching instances when a
+    # descriptor is received.
+    for instance in unique_instances:
+        while True:
+            try:
+                instance.fetch_descriptor()
+            except stem.SocketClosed:
+                logger.error("Failed to fecth descriptor, socket "
+                             "is closed")
+                util.reauthenticate(controller, logger)
+            else:
+                break
 
 
 class Instance(object):
@@ -135,3 +143,18 @@ class Instance(object):
         else:
             logger.debug("Introduction points for instance %s.onion matched "
                          "the cached set.", self.onion_address)
+
+    def __eq__(self, other):
+        """
+        Instance objects are equal if they have the same onion address.
+        """
+        if isinstance(other, Instance):
+            return self.onion_address == other.onion_address
+        else:
+            return False
+
+    def __hash__(self):
+        """
+        Define __hash__ method allowing for set comparison between instances.
+        """
+        return hash(self.onion_address)
