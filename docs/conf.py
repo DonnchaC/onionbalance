46a282823c6844c63b48978b34bcde0b08f7440e
Donncha O'Cearbhaill <donncha@donncha.is>
2017-05-02 18:06:13 +0200

    Add changelog and bump to version 0.1.8

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import sphinx.environment
 from docutils.utils import get_source_line
 
 # Documentation configuration
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 __author__ = "Donncha O'Cearbhaill"
 __contact__ = "donncha@donncha.is"
 
